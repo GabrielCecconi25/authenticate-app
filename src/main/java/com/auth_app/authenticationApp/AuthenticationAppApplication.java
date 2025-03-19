8e6351b2bcf03fea5668fd1982605d17e4432760
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-19 11:53:28 -0300

    feet: first commit, init authenticationApp

---
+++ src/main/java/com/auth_app/authenticationApp/AuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.auth_app.authenticationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth_app/authenticationApp/AuthenticationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth_app.authenticationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
