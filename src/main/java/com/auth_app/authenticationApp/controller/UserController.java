6e68f9f1aaf3f30b0f4d0124d1571014a709a636
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-21 15:33:40 -0300

    fix: communication with aService, ExceptionHandler to Controller, parameterization for statusCode for paths

--- src/main/java/com/auth_app/authenticationApp/controller/UserController.java
+++ src/main/java/com/auth_app/authenticationApp/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.auth_app.authenticationApp.controller;
 
-import org.hibernate.mapping.Any;
+import com.auth_app.authenticationApp.exception.ValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import com.auth_app.authenticationApp.model.User;
+import com.auth_app.authenticationApp.serivice.UserService;
 
 import java.util.Map;
 
@@ -11,24 +15,33 @@ import java.util.Map;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public String UserGetAll() {
         return "Teste";
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public String UserGetId(@PathVariable ("id") String id) {
         return id;
     }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public User UserPost(@RequestBody Map<String, String> body) {
-        String name = body.get("name");
-        String email = body.get("email");
-        String password = body.get("password");
-        Integer idade = Integer.parseInt(body.get("idade"));
-
-        return new User(name, email, password, idade);
+    public User UserPost(@RequestBody Map<String, String> data) {
+        return userService.userDataCreate(data);
     }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException ex) {
+        // Variável resposta de erro
+        Map<String, String> response = Map.of("error", ex.getMessage());
+
+        return ResponseEntity.status(ex.getStatus()).body(response);
+    }
+
 }
