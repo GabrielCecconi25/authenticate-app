14f49fc626604791a4e076ec2ec2caeea9772abb
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-21 17:25:26 -0300

    feet: adding save function for POST Users in Database

--- src/main/java/com/auth_app/authenticationApp/repository/UserRepository.java
+++ src/main/java/com/auth_app/authenticationApp/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package com.auth_app.authenticationApp.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.auth_app.authenticationApp.model.User;
+
+@Repository
 public class UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(User user) {
+        String sql = "INSERT INTO users (name, age, email, password) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, user.name, user.age, user.getEmail(), user.getPassword());
+    }
 }
