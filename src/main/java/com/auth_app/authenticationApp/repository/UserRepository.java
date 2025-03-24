b4e50b3c2887a41e3e4107471d870af7556ea580
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-24 16:57:37 -0300

    feet: Update user in repository, style: Adding comments to Repository for methods

--- src/main/java/com/auth_app/authenticationApp/repository/UserRepository.java
+++ src/main/java/com/auth_app/authenticationApp/repository/UserRepository.java
@@ -7,6 +7,7 @@ import com.auth_app.authenticationApp.model.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -27,18 +28,45 @@ public class UserRepository {
         );
     }
 
-    public void save(User user) {
-        String sql = "INSERT INTO users (name, age, email, password) VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, user.getName(), user.getAge(), user.getEmail(), user.getPassword());
-    }
 
+    // GET por ID
     public User findOne(String email) {
         String sql = "SELECT name, age, email FROM users WHERE email = ?";
         return jdbcTemplate.queryForObject(sql, this::mapRowToUser, email);
     }
 
+    // GET All
     public List<User> findAll () {
         String sql = "SELECT name, age, email FROM users";
         return jdbcTemplate.query(sql, this::mapRowToUser);
+    }
+
+    // POST
+    public void save(User user) {
+        String sql = "INSERT INTO users (name, age, email, password) VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, user.getName(), user.getAge(), user.getEmail(), user.getPassword());
+    }
+
+    // PATCH
+    public void update(User user) {
+        // Construir o Update de forma dinamica
+        StringBuilder sql = new StringBuilder("UPDATE users SET ");
+        List<Object> params = new ArrayList<>();
+
+        if (user.getName() != null) {
+            sql.append("name = ?, ");
+            params.add(user.getName());
+        }
+
+        if (user.getAge() > 0) {
+            sql.append("age = ?, ");
+            params.add(user.getAge());
+        }
+
+        sql.delete(sql.length() - 2, sql.length());
+        sql.append(" WHERE email = ?");
+        params.add(user.getEmail());
+
+        jdbcTemplate.update(sql.toString(), params.toArray());
     }
 }
