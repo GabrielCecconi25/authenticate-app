a2e54e23f9fa27921e352a56ad390e21725ea5c6
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-27 18:11:33 -0300

    feat: Adding class to remove null password from user in return from GET methods

---
+++ src/main/java/com/auth_app/authenticationApp/model/UserDTO.java
@@ -0,0 +1,40 @@
+package com.auth_app.authenticationApp.model;
+
+public class UserDTO {
+    private String name;
+    private int age;
+    private String email;
+
+    public UserDTO(String name, int age, String email) {
+        setName(name);
+        setAge(age);
+        setEmail(email);
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
