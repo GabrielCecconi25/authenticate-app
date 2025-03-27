fc4e1228542d22e5fb911319005111f5ab639087
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-27 18:10:11 -0300

    fix: Resolving issue with spring security

--- src/main/java/com/auth_app/authenticationApp/configuration/SecurityConf.java
+++ src/main/java/com/auth_app/authenticationApp/configuration/SecurityConf.java
@@ -2,35 +2,51 @@ package com.auth_app.authenticationApp.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConf {
+
+
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // 🔹 Forma correta no Spring Security 6+
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/**").permitAll() // Permite acesso à rota "/user" sem autenticação
-                        .anyRequest().authenticated()
-                )
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/user").permitAll()
+                .requestMatchers("/user/**").permitAll()
+                .anyRequest().permitAll()
+            )
+            .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("{noop}password") // `{noop}` indica que não há hash na senha
+        var user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
