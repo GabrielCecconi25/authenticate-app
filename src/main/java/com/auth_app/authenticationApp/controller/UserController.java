2648e71a3941f00530867e37da32f1678c8a3c2e
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-27 14:12:38 -0300

    feat: Exception to Validate params in User class

--- src/main/java/com/auth_app/authenticationApp/controller/UserController.java
+++ src/main/java/com/auth_app/authenticationApp/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.auth_app.authenticationApp.controller;
 
 import com.auth_app.authenticationApp.exception.ValidationException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import com.auth_app.authenticationApp.model.User;
-import com.auth_app.authenticationApp.serivice.UserService;
+import com.auth_app.authenticationApp.service.UserService;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -18,30 +22,32 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public String UserGetAll() {
-        return "Teste";
-    }
-
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public String UserGetId(@PathVariable ("id") String id) {
-        return id;
-    }
-
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED) // HTTP 201
-    public User UserPost(@RequestBody Map<String, String> data) {
+    public User UserPost(@Valid @RequestBody User data) {
         return userService.userDataCreate(data);
     }
 
+    // Exception Validation
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(ValidationException ex) {
         // Variável resposta de erro
         Map<String, String> response = Map.of("error", ex.getMessage());
-
         return ResponseEntity.status(ex.getStatus()).body(response);
+    }
+
+    // Exception para erros de validação de parametros na classe User
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidateException(MethodArgumentNotValidException ex) {
+        // Variável resposta dos erros de validação de parametros
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST); // HTTP 400
     }
 
 }
