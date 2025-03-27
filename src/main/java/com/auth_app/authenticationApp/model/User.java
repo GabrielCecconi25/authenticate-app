64e85200b3829ceb408d7ca0d4edb7896ffb8e83
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-27 14:13:21 -0300

    feat: Adding validation to parameters and adding new Constructors

--- src/main/java/com/auth_app/authenticationApp/model/User.java
+++ src/main/java/com/auth_app/authenticationApp/model/User.java
@@ -1,10 +1,23 @@
 package com.auth_app.authenticationApp.model;
 
+import jakarta.validation.constraints.*;
+
 public class User {
+    @NotNull(message = "Name cannot be null")
     private String name;
+
+    @Min(value = 1, message="Age must be greater than zero")
     private int age;
+
+    @Email(message = "Email format Invalid")
+    @NotNull(message = "Email cannot be null")
     private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    // Constructor para desserialização
+    public User() {}
 
     public User(String name, int age, String email, String password) {
         setName(name);
@@ -13,10 +26,21 @@ public class User {
         setPassword(password);
     }
 
-    // Construtor sem a senha para GET user
+    // Construtor sem a senha
     public User(String name, int age, String email) {
         setName(name);
         setAge(age);
+        setEmail(email);
+    }
+
+    // Contructor senha
+    public User(String email, String password) {
+        setEmail(email);
+        setPassword(password);
+    }
+
+    // Constructor email
+    public User(String email) {
         setEmail(email);
     }
 
